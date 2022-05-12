c00d0e8858e6466ebe9b98abe1769d81ab0b0c6b
Pure-Peace <940857703@qq.com>
2022-05-13 06:06:27 +0800

    test sdk

---
+++ test/utils/melos-nft.ts
@@ -0,0 +1,68 @@
+import {sendTransaction, executeScript} from 'flow-cadut';
+import {getAuthAccount, scriptCode, txCode} from './helpers';
+
+const addressMap = {
+  NonFungibleToken: '0xf8d6e0586b0a20c7',
+  MelosNFT: '0xf8d6e0586b0a20c7',
+};
+
+/**
+ * Setups Melos collection on account and exposes public capability.
+ * @param {string} account - account address
+ * */
+export const setupMelosOnAccount = async (account: string) => {
+  const {auth} = await getAuthAccount(account);
+
+  return sendTransaction({code: txCode('melos-nft/setup_account'), payer: auth, addressMap});
+};
+
+/**
+ * Returns Melos supply.
+ * @throws Will throw an error if execution will be halted
+ * */
+export const getMelosSupply = async () => {
+  return executeScript({code: scriptCode('melos-nft/get_melos_supply'), addressMap});
+};
+
+/**
+ * Mints Melos to **recipient**.
+ * */
+export const mintMelos = async (recipient: string) => {
+  const {auth} = await getAuthAccount('emulator-account');
+
+  return sendTransaction({code: txCode('melos-nft/mint_melos_nft'), args: [recipient], payer: auth, addressMap});
+};
+
+/**
+ * Transfers Melos NFT with id equal **itemId** from **sender** account to **recipient**.
+ * @param {string} sender - sender address
+ * @param {string} recipient - recipient address
+ * @param {UInt64} itemId - id of the item to transfer
+ * */
+export const transferMelos = async (sender: string, recipient: string, itemId: number) => {
+  const {auth} = await getAuthAccount(sender);
+
+  return sendTransaction({
+    code: txCode('melos-nft/transfer_melos_nft'),
+    args: [recipient, itemId],
+    payer: auth,
+    addressMap,
+  });
+};
+
+/**
+ * Returns the Melos NFT with the provided **id** from an account collection.
+ * @param {string} account - account address
+ * @param {UInt64} itemID - NFT id
+ * */
+export const getMelos = async (account: string, itemID: number) => {
+  return executeScript({code: scriptCode('melos-nft/get_melos'), args: [account, itemID], addressMap});
+};
+
+/**
+ * Returns the number of Melos in an account's collection.
+ * @param {string} account - account address
+ * */
+export const getMelosCount = async (account: string) => {
+  return executeScript({code: scriptCode('melos-nft/get_collection_length'), args: [account], addressMap});
+};
