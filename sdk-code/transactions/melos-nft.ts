5494be1385aa25a2b9c617a0ad3e75648b3d4891
Pure-Peace <940857703@qq.com>
2022-06-18 01:07:25 +0800

    add removeCollection

--- sdk-code/transactions/melos-nft.ts
+++ sdk-code/transactions/melos-nft.ts
@@ -110,14 +110,10 @@ import NonFungibleToken from "../../contracts/core/NonFungibleToken.cdc"
 import MelosNFT from "../../contracts/MelosNFT.cdc"
 
 
-// Setup storage for MelosNFT on signer account
-//
 transaction {
     prepare(acct: AuthAccount) {
-        if acct.borrow<&MelosNFT.Collection>(from: MelosNFT.CollectionStoragePath) == nil {
-          let collection <- acct.load<@MelosNFT.Collection>(from: MelosNFT.CollectionStoragePath)
-          destroy collection
-        }
+      let collection <- acct.load<@AnyResource>(from: MelosNFT.CollectionStoragePath)
+      destroy collection
     }
 }
 `,
