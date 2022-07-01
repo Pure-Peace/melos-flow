74f69bac61239d64002ba078eb8848195ed8ea5e
pure-peace <940857703@qq.com>
2022-07-01 11:25:15 +0800

    UPDATE

--- sdk-code/transactions/index.cjs
+++ sdk-code/transactions/index.cjs
@@ -1,11 +1,9 @@
 const common = require('./common');
-const melosMarketplace = require('./melos-marketplace');
 const melosMarketplaceTemplates = require('./melos-marketplace-templates');
 const melosNft = require('./melos-nft');
 
 module.exports = {
   common,
-  melosMarketplace,
   melosMarketplaceTemplates,
   melosNft,
 };
--- sdk-code/transactions/index.mjs
+++ sdk-code/transactions/index.mjs
@@ -1,11 +1,9 @@
 import common from './common';
-import melosMarketplace from './melos-marketplace';
 import melosMarketplaceTemplates from './melos-marketplace-templates';
 import melosNft from './melos-nft';
 
 export default {
   common,
-  melosMarketplace,
   melosMarketplaceTemplates,
   melosNft,
 };
